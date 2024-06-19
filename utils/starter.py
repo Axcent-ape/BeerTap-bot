a1a6ec19b7c5d4698f36b6643192403b815cdf54
Axcent <97802272+Axcent-ape@users.noreply.github.com>
2024-06-20 00:13:12 +0300

    Add files via upload

---
+++ utils/starter.py
@@ -0,0 +1,45 @@
+import os
+import random
+from utils.beer import BeerTap
+from utils.core import logger
+import datetime
+import pandas as pd
+from utils.core.telegram import Accounts
+from aiohttp.client_exceptions import ContentTypeError
+import asyncio
+from data import config
+
+
+async def start(thread: int, session_name: str, phone_number: str, proxy: [str, None]):
+    beer = BeerTap(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)
+    account = session_name + '.session'
+
+    if await beer.login():
+        logger.success(f"Thread {thread} | {account} | Login")
+        while True:
+            await asyncio.sleep(0.9)
+
+            new_balance = await beer.battery_taps(random.uniform(*config.PINTS))
+            logger.success(f"Thread {thread} | {account} | Poured beer! Balance: {new_balance}")
+
+    await beer.logout()
+
+
+async def stats():
+    accounts = await Accounts().get_accounts()
+
+    tasks = []
+    for thread, account in enumerate(accounts):
+        session_name, phone_number, proxy = account.values()
+        tasks.append(asyncio.create_task(BeerTap(session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy).stats()))
+
+    data = await asyncio.gather(*tasks)
+
+    path = f"statistics/statistics_{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv"
+    columns = ['Phone number', 'Name', 'Balance', 'Leaderboard', 'Referrals reward', 'Referrals', 'Referral link', 'Proxy (login:password@ip:port)']
+
+    if not os.path.exists('statistics'): os.mkdir('statistics')
+    df = pd.DataFrame(data, columns=columns)
+    df.to_csv(path, index=False, encoding='utf-8-sig')
+
+    logger.success(f"Saved statistics to {path}")
